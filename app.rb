65721bb6faac48991a02c733b9539d65b5b3ebb5
Jas Spencer <jas.spencer414@gmail.com>
2020-01-29 01:26:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+    enable :Sessions
+    set :session_secret, "TAandJ"
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    session[:item] = params[:items]
+    @session = session
+    erb :checkout
+  end
+end
+
 end
